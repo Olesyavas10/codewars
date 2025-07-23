413072cfd29df18af275ce8256094262b6170b72
OlesyaVas <olesyavas925@yandex.ru>
2025-07-23 12:16:11 +0700

    Switch-It-Up added

---
+++ 8lvl/Switch-it-Up!.js
@@ -0,0 +1,41 @@
+/* When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+
+Input: 1
+
+Output: "One".
+
+If your language supports it, try using a switch statement. */
+
+//Solution:
+function switchItUp(number){
+if(number===0){
+  return "Zero";
+  }
+if(number===1){
+  return "One";
+  }
+if(number===2){
+  return "Two";
+  }
+  if(number===3){
+  return "Three";
+  }
+  if(number===4){
+  return "Four";
+  }
+  if(number===5){
+  return "Five";
+  }
+  if(number===6){
+  return "Six";
+  }
+  if(number===7){
+  return "Seven";
+  }
+  if(number===8){
+  return "Eight";
+  }
+  if(number===9){
+  return "Nine";
+  }
+}
