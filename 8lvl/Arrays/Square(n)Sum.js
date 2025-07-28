2c14aa0056221752390c0e18bf79afff4cf22bd0
OlesyaVas <olesyavas925@yandex.ru>
2025-07-28 21:02:36 +0700

    DuckDuckGoose added

---
+++ 8lvl/Arrays/Square(n)Sum.js
@@ -0,0 +1,5 @@
+/* Complete the square sum function so that it squares each number passed into it and then sums the results together. */
+function squareSum(numbers){
+  let SqSum=0
+return SqSum=numbers.reduce((acc,num)=>acc+num*num, acc=0)
+}
