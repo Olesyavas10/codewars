b8ad775bbb3e79a67dfa55f616e50252683f073e
OlesyaVas <olesyavas925@yandex.ru>
2025-07-28 16:11:44 +0700

    ColorGhos added

---
+++ 8lvl/ColorGhost.js
@@ -0,0 +1,12 @@
+/* Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+ */
+let Ghost = function() {
+  const colors = ["white", "yellow", "purple", "red"];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    this.color = colors[randomIndex];
+};
