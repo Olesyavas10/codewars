3eab8219f5714180a5bdab882693187f5c75fbbf
OlesyaVas <olesyavas925@yandex.ru>
2025-07-28 19:06:48 +0700

    Beginner - Reduce but Grow added

---
+++ 8lvl/Beginner - ReducebutGrow.js
@@ -0,0 +1,7 @@
+/* Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+ */
+function grow(x){
+  let multiply=0;
+return multiply=x.reduce((acc,mult)=>acc*mult, acc=1)
+}
