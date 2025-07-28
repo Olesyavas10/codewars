5462e832baa2b17e8d1cc1e61acd22c56aa1bd1c
OlesyaVas <olesyavas925@yandex.ru>
2025-07-28 21:56:42 +0700

    Removing Elements added

---
+++ 8lvl/Arrays/RemovingElements.js
@@ -0,0 +1,13 @@
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element. */
+function removeEveryOther(arr){
+  let newArr=[]
+  for (let i=0; i<arr.length; i++)
+    {
+      if(i%2===0)
+        {
+          newArr.push(arr[i])
+        }
+     
+    }
+   return newArr
+}
