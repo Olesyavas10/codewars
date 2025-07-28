4292bf05aa8458be71f9a5b9cfa3e1ff09e8c793
OlesyaVas <olesyavas925@yandex.ru>
2025-07-28 16:27:05 +0700

    Hello, Name or World! added

--- 8lvl/ColorGhost.js
+++ 8lvl/ColorGhost.js
@@ -7,6 +7,6 @@ Ghost objects are given a random color attribute of "white" or "yellow" or "purp
  */
 let Ghost = function() {
   const colors = ["white", "yellow", "purple", "red"];
-    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomIndex = Math.floor(Math.random() * colors.length);// округление(рандомно от 0 до 1 * длину массива с цветами)
     this.color = colors[randomIndex];
 };
